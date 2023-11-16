d5cbe9fc849171a5bf3a5934cfd873c105a15012
Natalie Kershaw <nakersha@microsoft.com>
2023-11-16 17:27:59 +0000

    Align genai script with others

--- run_llama_genai_ort.py
+++ run_llama_genai_ort.py
@@ -1,6 +1,7 @@
-import onnxruntime_genai as og
+import datetime
 import numpy as np
 from transformers import LlamaTokenizer
+import onnxruntime_genai as og
 
 
 #device_type = og.DeviceType.CPU
@@ -11,48 +12,57 @@ tokenizer = LlamaTokenizer.from_pretrained('meta-llama/Llama-2-7b-hf')
 
 print("Loading model...")
 
-#model=og.Llama_Model("models/meta-llama/Llama-2-7b-hf-chat/rank_0_TinyLlama-1.1B-Chat-V0.3_decoder_merged_model_int4.onnx", device_type)
-model=og.Llama_Model("models/meta-llama/Llama-2-7b-hf-chat/rank_0_Llama-2-7b-chat-hf_decoder_merged_model_fp16.onnx", device_type)
-#model=og.Llama_Model("models/meta-llama/Llama-2-7b-hf-chat/rank_0_Llama-2-7b-chat-hf_decoder_merged_model_fp32.onnx", device_type)
+#model=og.Llama_Model("models/PY007/TinyLlama-1.1B-Chat-V0.3/rank_0_TinyLlama-1.1B-Chat-V0.3_decoder_merged_model_int4.onnx", device_type)
+model=og.Llama_Model("models/meta-llama/Llama-2-7b-chat-hf/rank_0_Llama-2-7b-chat-hf_decoder_merged_model_fp16.onnx", device_type)
+#model=og.Llama_Model("models/meta-llama/Llama-2-7b-chat-hf/rank_0_Llama-2-7b-chat-hf_decoder_merged_model_fp32_opt.onnx", device_type)
 
 print("Model loaded")
 
 
 # Keep asking for input prompts in an loop
-while True:
-    text = input("Input:")
-    input_tokens = tokenizer.encode(text, return_tensors='np')
+prompt = "I like walking my cute dog"
+input_ids = tokenizer.encode(prompt, return_tensors='np')
 
-    params=og.SearchParams()
-    params.max_length = 128
-    params.batch_size = input_tokens.shape[0]
-    params.sequence_length = input_tokens.shape[1]
-    params.input_ids = input_tokens
-    params.vocab_size = model.GetVocabSize()
-    params.eos_token_id = tokenizer.eos_token_id
-    params.pad_token_id = tokenizer.pad_token_id if tokenizer.pad_token_id is not None else params.eos_token_id
+params=og.SearchParams()
+params.max_length = 128
+params.batch_size = input_ids.shape[0]
+params.sequence_length = input_ids.shape[1]
+params.input_ids = input_ids
+params.vocab_size = model.GetVocabSize()
+params.eos_token_id = tokenizer.eos_token_id
+params.pad_token_id = tokenizer.pad_token_id if tokenizer.pad_token_id is not None else params.eos_token_id
 
-    search=og.GreedySearch(params, model.DeviceType)
-    state=og.Llama_State(model, search.GetSequenceLengths(), params)
+search=og.GreedySearch(params, model.DeviceType)
 
-    print("Output:")
+start_time = datetime.datetime.now()  
 
-    print(text, end='', flush=True)
-    while not search.IsDone():
-        search.SetLogits(state.Run(search.GetSequenceLength(), search.GetNextTokens()))
+state=og.Llama_State(model, search.GetSequenceLengths(), params)
 
-        # search.Apply_MinLength(1)
-        # search.Apply_RepetitionPenalty(1.0)
+generate_ids = []
+while not search.IsDone():
+    search.SetLogits(state.Run(search.GetSequenceLength(), search.GetNextTokens()))
 
-        search.SampleTopP(0.9, 0.6)
+    # search.Apply_MinLength(1)
+    # search.Apply_RepetitionPenalty(1.0)
 
-        # Print each token as we compute it, we have to do some work to get newlines & spaces to appear properly:
-        word=tokenizer.convert_ids_to_tokens([search.GetNextTokens().GetArray()[0]])[0]
-        if word=='<0x0A>':
-          word = '\n'
-        if word.startswith('▁'):
-          word = ' ' + word[1:]
-        print(word, end='', flush=True)
+    search.SampleTopP(0.9, 0.6)
 
-    print()
-    print()
+    # Get the next generated token
+    generate_ids.append(search.GetNextTokens().GetArray()[0])
+
+end_time = datetime.datetime.now()
+
+print(generate_ids)
+
+output = tokenizer.batch_decode(generate_ids, skip_special_tokens=False, clean_up_tokenization_spaces=False)[0] 
+
+print(prompt, end=' ', flush=True)
+print(output, end='', flush=True)
+print("\n")
+
+new_tokens = len(generate_ids)
+num_tokens = input_ids.shape[1] + new_tokens
+seconds = (end_time - start_time).total_seconds()
+print(f"GenAI + ONNX Runtime, {num_tokens}, {new_tokens}, {round(seconds, 2)}, {round(num_tokens / seconds, 1)}, {round(new_tokens / seconds, 1)}")
+
+
--- run_llama_ort.py
+++ run_llama_ort.py
@@ -1,6 +1,6 @@
 import onnxruntime as ort
 
-sess = ort.InferenceSession("TinyLlama-1.1B-step-50K-105b-onnx/decoder_with_past_model.onnx", providers=["CPUExecutionProvider"])
+sess = ort.InferenceSession("models/meta-llama/Llama-2-7b-chat-hf/rank_0_Llama-2-7b-chat-hf_decoder_merged_model_fp16.onnx", providers=["CUDAExecutionProvider"])
 input_names = list(map(lambda inpt: inpt.name, sess.get_inputs()))
 print(input_names)
 input_shapes = list(map(lambda inpt: inpt.shape, sess.get_inputs()))
